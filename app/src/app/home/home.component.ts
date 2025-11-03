4c2963d4f3de8fc3c08ac209c1035bf9b73a1e97
Todd Qualls <tqualls@gmail.com>
2025-11-02 21:06:28 -0500

    Remove unused PrintableChecklistComponent import from home

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -2,12 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { PrintableChecklistComponent } from '../printable-checklist/printable-checklist.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, MatCardModule, MatButtonModule, PrintableChecklistComponent],
+  imports: [RouterLink, MatCardModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
